219398dd7596b36d19446c33a65d9746318b524c
Danil Chekantsev <chekantsev.danil2@gmail.com>
2024-12-11 08:01:02 +0300

    Initial commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Danil Chekantsev on 11.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
