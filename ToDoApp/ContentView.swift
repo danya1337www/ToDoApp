742c4b03bfb889380f0db68839c4c8391296ca97
Danil Chekantsev <chekantsev.danil2@gmail.com>
2024-12-12 16:32:24 +0300

    Adding a static list of to-do items to ContentView. Adding a largeTitle HStack.

--- ToDoApp/ContentView.swift
+++ ToDoApp/ContentView.swift
@@ -9,11 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        HStack {
+            Image(systemName: "star.fill")
+                .foregroundStyle(.yellow)
+                .padding(30)
+            Text("Favorite")
+                .padding(-11)
+            Spacer()
+            Text("do it")
+                .font(.largeTitle)
+                .bold()
+            Spacer()
+            Button("Add") {
+                print("Add")
+            }
+            .padding(30)
+        }
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            List {
+                Text("do it")
+                Text("do it")
+                Text("do it")
+                
+            }
+            .padding()
+            .background(Color.blue.opacity(0.2))
+            .cornerRadius(20)
+            .scrollContentBackground(.hidden)
         }
         .padding()
     }
